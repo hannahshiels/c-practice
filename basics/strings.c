bbee31a435e1b7a1484d2e14a0e407fe71fb1566
hannahshiels <thebelfastflygon@gmail.com>
2022-03-09 13:41:51 +0100

    add palindrome checker

--- basics/strings.c
+++ basics/strings.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 
 char *add(char *word, char *word2);
+int isPalindrome(char *word);
+void printResult(int result);
 
 int main()
 {
@@ -33,6 +35,15 @@ int main()
 
     char *mixed = add(sentence, sentence2);
     printf("%s\n", mixed);
+
+    char *randomWord = "banana";
+    printf("%s %s %s? ", "Is", randomWord, "a palindrome" );
+    int result = isPalindrome(randomWord);
+    printResult(result);
+    char *randomWord2 = "abba";
+    printf("%s %s %s? ", "Is", randomWord2, "a palindrome" );
+    result = isPalindrome(randomWord2);
+    printResult(result);
     return 0;
 }
 
@@ -52,4 +63,24 @@ char *add(char *word, char *word2)
         fullSentence++;
     }
     return ptr;
+}
+
+void printResult(int result){
+    if(result == 0){
+        printf("%s\n", "Yes, it is." );
+    } else {
+        printf("%s\n", "No, it isn't." );
+    }
+}
+
+int isPalindrome(char *word){
+    int count = 0;
+
+    for(int i = strlen(word)-1; i >= 0; i--){
+        if(word[i] != word[count]){
+            return 1;
+        }
+        count++;
+    }
+    return 0;
 }
