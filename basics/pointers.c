8b4a71dd0fc093d683b56dd1fd73f9557ee5406a
Hannah Shiels <thebelfastflygon@gmail.com>
2022-03-09 11:04:30 +0100

    add pointer example

--- basics/pointers.c
+++ basics/pointers.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
+void changeVal(int *x){
+    *x = 2;
+}
+
 int main(){
+    int x = 12;
+    int *y =&x;
+    printf("%d\n", *y);
+    changeVal(y);
+    printf("%d\n", *y);
     return 0;
 }
