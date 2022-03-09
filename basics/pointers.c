23fe4e250c9c4f443254a13bd9e7a8ecd8857365
Hannah Shiels <thebelfastflygon@gmail.com>
2022-03-09 11:27:05 +0100

    change strings

--- basics/pointers.c
+++ basics/pointers.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
-void changeVal(int *x){
-    *x = 2;
-}
+void changeVal(int *x);
+void changeString(char *str);
+
+char arr[] = "Banana";
+
 
 int main(){
     int x = 12;
@@ -10,5 +13,24 @@ int main(){
     printf("%d\n", *y);
     changeVal(y);
     printf("%d\n", *y);
+    char *str = &arr[0];
+    printf("%s\n", str);
+    changeString(str);
+    printf("%s\n", str);
     return 0;
+}
+
+void changeString(char *str){
+    for(int i = 0; i < strlen(arr); i++){
+        *str = '#';
+        str++;
+    }
+    for(int i = 0; i < strlen(arr); i++){
+        str--;
+    }
+}
+
+
+void changeVal(int *x){
+    *x = 2;
 }
